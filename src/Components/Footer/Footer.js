b80cfc364ee9bf5f46d89838ac713d665ab6b970
nurbek19957072 <nurbek19957072@gmail.com>
2023-09-22 14:36:32 +0600

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,6 +6,8 @@ const Footer = () => {
         <footer>
             <div><input type="text" name="" id="" /></div>
             <div><button>Send</button></div>
+            <div><button>Send</button></div>
+            <div><button>Send</button></div>
         </footer>
     </div>
   )
